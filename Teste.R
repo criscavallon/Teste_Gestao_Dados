507f3a5a85f77a17f0e51cb93204108914a12cfa
CrisCavallon <cris.cavallon@sempreceub.com>
2020-08-26 12:17:59 -0300

    1st commit

---
+++ Teste.R
@@ -0,0 +1,8 @@
+
+# importando a base
+dados = read.csv("Cristiana Campos Cavallon - PlayTennis.csv", sep = ";", na.strings = "", stringsAsFactors = T)
+
+summary(dados)
+
+# Tratar NA em dados categóricos
+
